6db328bfa22eb506e450f029377953239c936aaf
Kacper Pikulski <pikulak1@gmail.com>
2016-10-09 19:13:12 +0200

    new file:   pitcp.py

---
+++ pitcp.py
@@ -0,0 +1,9 @@
+﻿#!c:/python34/python.exe
+import socket
+import errors
+from base64 import b64encode
+import json
+def send(headers, data, sock):
+    headers = b64encode(json.dumps(headers).encode('utf8'))
+    sock.sendall(headers)
+    sock.sendall(json.dumps(data).encode('utf8'))
