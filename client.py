75a0ab32b5a4dfb91b740e84cdeb881be58f7f14
Kacper Pikulski <pikulak1@gmail.com>
2016-10-09 19:41:53 +0200

    modified:   client.py

--- client.py
+++ client.py
@@ -7,27 +7,32 @@ import json
 import sys
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.1.102',8002)) 
+sock.connect(('192.168.1.102', 8002))
+
+
 def prepareHeaders(len, flag, type):
     return {
-        'Length'   : len,
-        'Flag'     : flag,
-        'Date'     : str(datetime.now().strftime("%Y-%m-%d %H:%M")),
+        'Length': len,
+        'Flag': flag,
+        'Date': str(datetime.now().strftime("%Y-%m-%d %H:%M")),
         'Data-type': type,
     }
-   
+
+
 def receive(nothing):
     while True:
         message = sock.recv(1024).decode('utf8')
-        
+
         if message:
             print(message[3:])
-            
+
+
 def prepareData(**kwargs):
     data = {}
     for key, value in kwargs.items():
         data[key] = value
     return data
+
 
 def auth(name):
     data = prepareData(name=name)
@@ -39,16 +44,17 @@ def auth(name):
     else:
         print('Cant log-in:// Try again or type "quit"')
         return False
-    
+
 nameInp = input("Enter username:")
 auth(nameInp)
 nothing = 0
-start_new_thread(receive,(nothing,))
+start_new_thread(receive, (nothing,))
 while True:
-    inp = input(nameInp.strip()+">")
+    inp = input(nameInp.strip() + ">")
     data = prepareData(message=inp)
     headers = prepareHeaders(len(str(data)), 2, 'json')
-    
+
     send(headers, data, sock)
-    if inp == 'q':  break
+    if inp == 'q':
+        break
 sock.close()
