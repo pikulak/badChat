390c0700c91258d91e250d0e10517138744955e6
Kacper Pikulski <pikulak1@gmail.com>
2016-10-09 18:44:44 +0200

    Update server.py
    Added some comments

--- server.py
+++ server.py
@@ -10,21 +10,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('192.168.1.102',8000))
 sock.listen(50)
 quit = True
-#class ownSocket(object):
-#    def __init__(self, *p):
-#        self._sock = socket.socket(*p)
-#    def __getattr__(self, name):
-#        return getattr(self._sock, name)
-#    def __repr__(self):
-#        return "Socket object with: "+self._sock.getpeername()[0]+ str(self._sock.getpeername()[1])
-#def delUser(dict, value):
-#    for name, sock in dict.items():
-#        if sock.getpeername() == value:
-#            r = 1,sock.getpeername()[0], name
-#            del dict[name]
-#            return r
-#    return 0, value[0]
-list_connections = []
+list_connections = [] # shared between connection threads
+
 def delUser(dict, value): #value=ip address
     name = dict[value]
     try:
@@ -33,11 +20,7 @@ def delUser(dict, value): #value=ip address
         return True
     except KeyError:
         return False
-class User(object):
-    def __init__(self, name, ip):
-        self.name = name
-        self.ip = ip
-
+    
 class Server():
     def __init__(self, srvAddr):
         self.auth_users = {}
@@ -51,13 +34,13 @@ class Server():
     def auth(self, data, connection):
         global list_connections
         if data['name'] in self.auth_users:
-            print('Failed to login,existing user, ip=',connection.getpeername()[0],'user=',data['name'])
+            print('Failed to login,existing user, ip=',connection.getpeername()[0],'user=',data['name']) # SOME LOG STUFF
             return 'BAD', 'User exists'
         else:
             self.auth_users[data['name']] = connection
             self.auth_users[self.connStrRepr(connection)] = data['name']
             list_connections.append(connection)
-            print("Loged in:",data['name'])
+            print("Loged in:",data['name'])  # SOME LOG STUFF
             return 'OK!', 'Successful login'
     
     def connStrRepr(self, connection):
@@ -69,7 +52,7 @@ class Server():
         global list_connections
         for connection in list_connections:
             if connection != sender_connection:
-                print('sent to',connection)
+                print('sent to',connection)  # SOME LOG STUFF
                 connection.sendall(b'OK!'+ message.encode('utf8'))
         return 'OK!', 'Messages sent to all'
                 
@@ -119,7 +102,7 @@ class Server():
         if data == 'Disconnected by client':
             return data
         else:
-            print(data)
+            print(data)  # SOME DEBUG STUFF
         response = self.proccessData(data, flag, connection)
         connection.sendall(response[0].encode('utf8'))
         return response[1]
@@ -145,18 +128,18 @@ class Server():
                 msg = r
                 if msg == 'Messages sent to all':
                     name = self.auth_users[act_addr]
-                    print('Message from',name, 'to all')
+                    print('Message from',name, 'to all') # SOME DEBUG STUFF
                 if msg == 'Successful login':
                     name = self.auth_users[act_addr]
-                    print('Successful login from', act_addr, 'as', name)
+                    print('Successful login from', act_addr, 'as', name) # SOME DEBUG STUFF
                 if msg == 'User exists':
                     break
                 if msg == 'Disconnected by client':
                     break
             else:
-                print("Disconnected: ", connection.getpeername(),'code=', 0)
-                connection.sendall(b'BAD')
-                break
+                print("Disconnected: ", connection.getpeername(),'code=', 0) # SOME DEBUG STUFF
+                connection.sendall(b'BAD') # SOME DEBUG STUFF
+                break # SOME DEBUG STUFF
                 
         if msg == 'Successful login':
             name = self.auth_users[act_addr] # connection.getpeername[0] = ip addr
@@ -164,32 +147,34 @@ class Server():
                 if delUser(self.auth_users, act_addr):
                     list_connections.remove(connection)
                     msg = "Successful disconnected by server, user="+name+"(addr="+ act_addr + "), " + "code=1" # when disconnected with name
+                    print(msg) # SOME DEBUG STUFF
                     
         if msg == 'User exists':
             msg = "Successful disconnected by server, addr="+act_addr+ ", code=0" #while connected without name
-#       
+            print(msg) # SOME DEBUG STUFF
+            
         if msg == 'Disconnected by client':
             try:
                 name = self.auth_users[act_addr]
                 delUser(self.auth_users, act_addr)
                 list_connections.remove(connection)
-                print(msg, act_addr)
+                print(msg, act_addr) # SOME DEBUG STUFF
             except KeyError:
-                print(msg, act_addr)
+                print(msg, act_addr) # SOME DEBUG STUFF
         if msg == 'Bad data':
-            print('Bad data',act_addr)
-        connection.close()
+            print('Bad data',act_addr) # SOME DEBUG STUFF
+        connection.close() 
         
     def authUserPrinter(self):
         lenTmp = 0
         global list_connections
-        print('Initializing authUserPrinter...')
+        print('Initializing authUserPrinter...') # SOME DEBUG STUFF
         while True:
             sleep(0.5)
             now = len(list_connections)
             if lenTmp != now:
                 lenTmp = now
-                print(list_connections)
+                print(list_connections) # SOME DEBUG STUFF
     
     def run(self):
         self.createSocket()
@@ -197,6 +182,6 @@ class Server():
         start_new_thread(self.authUserPrinter,())
         while True:
             connection, none = self.sock.accept()
-            print('Connected! ', connection.getpeername())
+            print('Connected! ', connection.getpeername()) # SOME DEBUG STUFF
             start_new_thread(self.clientThread,(connection,))
 Server(('192.168.1.102', 8002)).run()
